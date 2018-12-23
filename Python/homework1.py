57252961d86179e9f075411729bc22b5a0d41868
Moosa Bonomali <gr8tech01@gmail.com>
2018-12-23 15:42:06 +0200

    Implemented Python Homework Assignment 1

---
+++ Python/homework1.py
@@ -0,0 +1,68 @@
+'''
+Python Homework Assignment 1
+Course: Python is Easy @ pirple
+Author: Moosa
+Email: gr8tech01@gmail.com
+
+Use variables to list characteristics of my favourite song
+'''
+
+# Name of the artist
+Artist = "Zain Bhikha"
+# Artist official website
+Website = "http://zainbhikha.com/"
+# Favourite Song title
+Song_title = "Forgive me when I whine"
+# Song length in minutes:seconds
+Duration = "2:35"
+# Studio which produced album
+Studio = "Zain Bhikha Studios"
+# Language of the song
+Language = "English"
+# Title of Album
+Album = "Mountains of Makkah"
+# URL for Album details
+Album_URL = "http://zainbhikha.com/album/08-mountains-of-makkah-2005/"
+# Year album was released
+Year = 2005
+# Song genre
+Genre = "Religion/R&B/Soul"
+# URL for song video
+Link= "https://www.youtube.com/watch?v=0TO4tWE3gSY"
+# URL to Apple music store
+itunes = "https://itunes.apple.com/us/album/336347122?i=336347257"
+# Lyrics for the song
+Lyrics =  r'''
+Today, upon a bus I saw a girl with golden hair,
+And in my heart I wished that I was just as fair
+When suddenly she rose to leave, I saw her hobble down the aisle.
+She had one leg and wore a crutch, but as she passed, a smile.
+Oh Allah Oh Allah , forgive me when I whine.
+I have 2 legs to walk upon and the world is mine.
+
+I stopped to buy some candy; i met a boy who had such charm.
+We talked he seemed so happy, if I were late, it'd do no harm.
+And as I left, he said to me, "Thank you, you've been so kind.
+It's nice to talk with folks like you. You see," he said, "I'm blind."
+Oh Allah Oh Allah , forgive me when I whine.
+I have 2 eyes to see the world and the world is mine.
+
+....
+'''
+
+print("Song\t\t",Song_title)
+print("Artist\t\t",Artist)
+print("Duration\t",Duration)
+print("Genre\t\t",Genre)
+print("Language\t",Language)
+print("Album\t\t",Album)
+print("Year\t\t",Year)
+print("Studio\t\t",Studio)
+print("Apple Music\t",itunes)
+print("URL\t\t",Album_URL)
+print("Video\t\t",Link)
+print("Website\t\t",Website)
+print()
+print("Lyrics")
+print(Lyrics)
+
