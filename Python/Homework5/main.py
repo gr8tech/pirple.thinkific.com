8380f541648bb841a1c6644658561370606ccdf0
Moosa Bonomali <gr8tech01@gmail.com>
2018-12-25 14:00:32 +0200

    Added edge case for value 3

--- Python/Homework5/main.py
+++ Python/Homework5/main.py
@@ -15,16 +15,27 @@ def FizzBuzzPrime():
 		Prints "Buzz", if the number is a multiple of 5
 		Prints "FizzBuzz", if the number is a multiple of 3 and 5, i.e 15
 		Prints "Prime", if the number is a prime number
+		Otherwise, it prints the number
+	Edge Case:
+		3 is a prime number and will also generate "Fizz"
+		Otherwise, any Fizz, Buzz or FizzBuzz cannot be a prime number
 	'''
 	for i in range(1,101):
 		if i % 15 == 0:
-			print(i,"FizzBuzz")
+			print("FizzBuzz")
 		elif i % 5 == 0:
-			print(i,"Buzz")
+			print("Buzz")
 		elif i % 3 == 0:
-			print(i,"Fizz")
-		if IsPrime(i):
-			print(i,"Prime")
+			print("Fizz")
+			if i == 3: # take care of the edge case
+				print("Prime")
+		elif IsPrime(i) and i > 1:
+			print("Prime")
+		else:
+			print(i)
+
+		
+		
 
 def IsPrime(value):
 	'''
