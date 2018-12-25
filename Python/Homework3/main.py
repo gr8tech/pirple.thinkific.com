975e366aaac8042d5d754e24012edf107c09ab20
Moosa Bonomali <gr8tech01@gmail.com>
2018-12-25 06:11:29 +0200

    Added Homework #3

---
+++ Python/Homework3/main.py
@@ -0,0 +1,40 @@
+'''
+Python Homework Assignment 3
+Course: Python is Easy @ pirple
+Author: Moosa
+Email: gr8tech01@gmail.com
+
+`If` statements; Comparison of numbers
+'''
+
+def te(num1, num2, num3):
+	'''
+	Functions checks if 2 or more of the given numbers are equal
+	Args:
+		num1, num2, num3
+		num1, num2, num3 can be an Integer or an Integer string
+	Returns:
+		True if 2 or more numbers are equal, False otherwise
+	Examples:
+		te(1,2,1) returns True
+		te(1,2,"1") returns True
+		te(1,2,3) return False
+	Logic:
+		There are three posibilities
+		1. num1 is equal to num2 OR
+		2. num1 is equal to num3 OR
+		3. num2 is equal to num3
+	'''
+	num1, num2, num3 = int(num1), int(num2), int(num3)
+	if (num1 == num2) or (num1 == num3) or (num2 == num3):
+		return True
+	else:
+		return False
+
+# Function tests
+# Returns False
+print(te(1,2,3))
+# Returns True
+print(te(1,2,1))
+#Returns True
+print(te("5",6,5)) 
